3905d426f127b7c7c08e5cf12216cde1cf6834da
Pablosky <pablo160896@gmail.com>
2016-10-16 22:06:48 +0200

    Acabado

--- app/src/main/java/com/example/pablosky/masviews/MainActivity.java
+++ app/src/main/java/com/example/pablosky/masviews/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.pablosky.masviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sePulsa(View v){
+        Toast.makeText(this,"Pulsado", Toast.LENGTH_SHORT).show();
     }
 }
