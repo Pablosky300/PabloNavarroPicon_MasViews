23eb00b11ab24a3f838d324429b1e037f081343e
Pablosky <pablo160896@gmail.com>
2016-10-18 12:24:11 +0200

    Acabado

--- app/src/main/java/com/example/pablosky/masviews/MainActivity.java
+++ app/src/main/java/com/example/pablosky/masviews/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.pablosky.masviews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText entrada = (EditText) findViewById(R.id.entrada);
+    private TextView salida = (EditText) findViewById(R.id.salida);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void sePulsa(View v){
-        Toast.makeText(this,"Pulsado", Toast.LENGTH_SHORT).show();
+        salida.setText(String.valueOf(
+                Float.parseFloat(entrada.getText().toString())*2.0));
     }
+
+
+    public void sePulsa0(View v){
+        entrada.setText(entrada.getText()+(String)v.getTag());
+    }
+
 }
